0fd2d2078c76a3885c1de9e715e823603a5b2ce9
sjgosai <sgosai@g.harvard.edu>
2021-01-06 12:52:57 -0500

    Create build_and_push.sh

---
+++ analysis/SG00__getting_started/src/build_and_push.sh
@@ -0,0 +1,48 @@
+%%sh
+  
+# The name of our algorithm
+algorithm_name=$1
+algorithm_version=$2
+dockerfile=${3:-Dockerfile}
+echo "Building:"
+echo "${algorithm_name}:${algorithm_version}"
+
+
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+region=${region:-us-east-1}
+
+echo "in ${region}"
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:${algorithm_version}"
+
+echo "tag: ${fullname}"
+
+# If the repository doesn't exist in ECR, create it.
+
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --region ${region} --no-include-email)
+
+# Get the login command from ECR in order to pull down the SageMaker PyTorch image
+$(aws ecr get-login --registry-ids 763104351884 --region ${region} --no-include-email)
+
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
+
+#cp ../common/data_io_utils.py code/common/
+
+echo $PWD
+docker build  -t ${algorithm_name} -f ${dockerfile} . --build-arg REGION=${region}
+docker tag ${algorithm_name} ${fullname}
+
+docker push ${fullname}
