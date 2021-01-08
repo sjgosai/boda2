9c82c2f2491390ea76cfcd067fad72b3beae9aa3
sjgosai <sgosai@g.harvard.edu>
2021-01-08 09:08:20 +0000

    Added some google storage interaction for Google AI Platform compat

--- analysis/SG00__getting_started/src/main.py
+++ analysis/SG00__getting_started/src/main.py
@@ -3,6 +3,8 @@ import sys
 import os
 import shutil
 
+import subprocess
+
 import torch
 from pytorch_lightning import Trainer
 
@@ -20,26 +22,47 @@ def main(args):
     
     trainer.fit(model, data)
     
-    _save_model(trainer, args.sagemaker_checkpoint)
+    _save_model(trainer, args.model_dir)
     
     return None
 
 def _save_model(trainer, model_dir, inference_script_dir=None):
-    trainer.save_checkpoint(os.path.join(model_dir, 'model.ckpt'))
-    os.makedirs(os.path.join(model_dir,'code'), exist_ok=True)
+    if 'gs://' in model_dir:
+        local_dir = '/tmp/output/model'
+    else:
+        local_dir = model_dir
+        
+    trainer.save_checkpoint(os.path.join(local_dir, 'model.ckpt'))
+    os.makedirs(os.path.join(local_dir,'code'), exist_ok=True)
     if inference_script_dir is None:
         script_dir = os.path.dirname(sys.argv[0])
     else:
         script_dir = inference_script_dir
     shutil.copy(os.path.join(script_dir,'./inference.py'), 
-                os.path.join(model_dir,'code'))
+                os.path.join(local_dir,'code'))
+    
+    if 'gs://' in model_dir:
+        subprocess.check_call(
+            ['gsutil', 'cp', '-r', local_dir, model_dir]
+                             )
+    else:
+        pass
+    
     return None
 
 def model_fn(model_dir):
+    if 'gs://' in model_dir:
+        local_dir = '/tmp/input/model'
+        subprocess.check_call(
+            ['gsutil', 'cp', '-r', model_dir, local_dir]
+                             )
+    else:
+        local_dir = model_dir
+
     # PTL friendly version
-    #return cifar10.MyNet.load_from_checkpoint(checkpoint_path=os.path.join(model_dir,'model.ckpt'))
+    #return cifar10.MyNet.load_from_checkpoint(checkpoint_path=os.path.join(local_dir,'model.ckpt'))
     # Hack to deal with setup
-    checkpoint = torch.load(os.path.join(model_dir,'model.ckpt'))
+    checkpoint = torch.load(os.path.join(local_dir,'model.ckpt'))
     new_model = cifar10.MyNet(**checkpoint['hyper_parameters'])
     new_model.setup('train')
     new_model.load_state_dict(checkpoint['state_dict'])
@@ -48,8 +71,8 @@ def model_fn(model_dir):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="CIFAR10", add_help=False)
-    parser.add_argument('--sagemaker_checkpoint', type=str, default=os.getenv('SM_MODEL_DIR'))
-    parser.add_argument('--pretrained_model', type=str, default=os.getenv('SM_CHANNEL_MODEL'))
+    parser.add_argument('--model_dir', type=str, default=os.getenv('MODEL_DIR'))
+    parser.add_argument('--pretrained_model', type=str, default=os.getenv('CHANNEL_MODEL'))
     
     parser = cifar10.MyDataModule.add_data_specific_args(parser)
     parser = cifar10.MyNet.add_model_specific_args(parser)
