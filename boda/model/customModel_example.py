8d26735af7f31a7a88a59471e0b540b7e1165547
Rodrigo Castro <rodrigo.i.castro@gmail.com>
2021-01-11 18:52:28 -0500

    Update customModel_example.py

--- boda/model/customModel_example.py
+++ boda/model/customModel_example.py
@@ -7,14 +7,11 @@ Created on Wed Dec 30 18:21:59 2020
 """
 
 import argparse
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import random_split, DataLoader, TensorDataset
 from torchsummary import summary
 import pytorch_lightning as pl
-from pytorch_lightning.metrics.functional import accuracy
 
 
 '''
@@ -33,6 +30,7 @@ class MPRAregressionModel(pl.LightningModule):
         parser.add_argument('--weightDecay', type=float, default=1e-8)
         parser.add_argument('--dropout', type=float, default=0.2)
         parser.add_argument('--optimizer', type=str, default='Adam')
+        parser.add_argument('--scheduler', type=bool, default=True)
         
         #input shape
         parser.add_argument('--seqLen', type=int, default=600)
@@ -62,61 +60,88 @@ class MPRAregressionModel(pl.LightningModule):
         parser.add_argument('--linearLayerLen2', type=int, default=10)
         
         args = parser.parse_args()
-        print(f'Model parameters: {vars(args)}')
+        print(f'Parser argumentss: {vars(args)}')
         return parser
     
-    def __init__(self, modelParams):
+    def __init__(self,
+                 LR=0.0005,
+                 momentum=0.9,
+                 weightDecay=1e-8,
+                 dropout=0.2,
+                 optimizer='Adam',
+                 scheduler=True,
+                 seqLen=600,
+                 numFeatures=4,
+                 targetLen=1,
+                 numChannles1=20,
+                 kernelSize1=6,
+                 stride1=3,
+                 padding1=0,
+                 dilation1=1,
+                 poolKernel1=4,
+                 poolStride1=2,
+                 numChannles2=10,
+                 kernelSize2=4,
+                 stride2=2,
+                 padding2=0,
+                 dilation2=1,
+                 poolKernel2=2,
+                 poolStride2=2,
+                 linearLayerLen1=50,
+                 linearLayerLen2=10, **kwargs):
+        
         super().__init__()
         #training params
-        self.LR = modelParams['LR']
-        self.momentum = modelParams['momentum']
-        self.weightDecay = modelParams['weightDecay']
-        self.dropout = modelParams['dropout']
-        self.optimizer = modelParams['optimizer']
+        self.LR = LR
+        self.momentum = momentum
+        self.weightDecay = weightDecay
+        self.dropout = dropout       
+        self.optimizer = optimizer
+        self.loss = nn.MSELoss()
         
         #input shape
-        self.seqLen = modelParams['seqLen']
-        self.numFeatures = modelParams['numFeatures']
-        self.targetLen = modelParams['targetLen']
+        self.seqLen = seqLen
+        self.numFeatures = numFeatures
+        self.targetLen = targetLen
         self.example_input_array = torch.rand(1, self.numFeatures, self.seqLen)
         
         #network params
-        self.numChannels1 = modelParams['numChannles1']
-        self.kernelSize1 = modelParams['kernelSize1']
-        self.stride1 = modelParams['stride1']
-        self.padding1 = modelParams['padding1']
-        self.dilation1 = modelParams['dilation1']
+        self.numChannels1 = numChannles1
+        self.kernelSize1 = kernelSize1
+        self.stride1 = stride1
+        self.padding1 = padding1
+        self.dilation1 = dilation1
         self.convLayerLen1 = self.conv1D_OutDim(L_in=self.seqLen, 
                                        kernel_size=self.kernelSize1,
                                        stride=self.stride1,
                                        padding=self.padding1,
                                        dilation=self.dilation1)
         
-        self.poolKernel1 = modelParams['poolKernel1']
-        self.poolStride1 = modelParams['poolStride1']
+        self.poolKernel1 = poolKernel1
+        self.poolStride1 = poolStride1
         self.poolLayerLen1 = self.conv1D_OutDim(L_in=self.convLayerLen1, 
                                        kernel_size=self.poolKernel1,
                                        stride=self.poolStride1)
         
-        self.numChannels2 = modelParams['numChannles2']
-        self.kernelSize2 = modelParams['kernelSize2']
-        self.stride2 = modelParams['stride2']
-        self.padding2 = modelParams['padding2']
-        self.dilation2 = modelParams['dilation2']
+        self.numChannels2 = numChannles2
+        self.kernelSize2 = kernelSize2
+        self.stride2 = stride2
+        self.padding2 = padding2
+        self.dilation2 = dilation2
         self.convLayerLen2 = self.conv1D_OutDim(L_in=self.poolLayerLen1, 
                                        kernel_size=self.kernelSize2,
                                        stride=self.stride2,
                                        padding=self.padding2,
                                        dilation=self.dilation2)    
         
-        self.poolKernel2 = modelParams['poolKernel2']
-        self.poolStride2 = modelParams['poolStride2']
+        self.poolKernel2 = poolKernel2
+        self.poolStride2 = poolStride2
         self.poolLayerLen2 = self.conv1D_OutDim(L_in=self.convLayerLen2, 
                                        kernel_size=self.poolKernel2,
                                        stride=self.poolStride2)
         
-        self.linearLayerLen1 = modelParams['linearLayerLen1']
-        self.linearLayerLen2 = modelParams['linearLayerLen2']
+        self.linearLayerLen1 = linearLayerLen1
+        self.linearLayerLen2 = linearLayerLen2
         
         #hidden layers
         self.convLayers = nn.Sequential(
@@ -174,7 +199,18 @@ class MPRAregressionModel(pl.LightningModule):
             optimizer = torch.optim.RMSprop(self.parameters(), lr=self.LR, alpha=0.99, eps=1e-8,
                                         weight_decay = self.weightDecay, momentum = self.momentum,
                                         centered=True)
-        return optimizer
+        
+        #needs a better way of passing the number of epochs
+        if self.scheduler:
+            lr_scheduler = {
+                'scheduler' : torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=Epochs,   
+                                                           eta_min=0.0000001, last_epoch=-1),
+                'name': 'learning_rate'
+                           }
+            return [optimizer], [lr_scheduler]
+        
+        else:
+            return optimizer
     
     
     #calculate the output length of a 1D-convolutional layer    
@@ -182,40 +218,41 @@ class MPRAregressionModel(pl.LightningModule):
     def conv1D_OutDim(L_in, kernel_size, stride, padding=0, dilation=1):
         return (L_in + 2*padding - dilation*(kernel_size - 1) - 1)//stride + 1     
     
+  
     
 #------------------------------- EXAMPLE --------------------------------------------------
 if __name__ == '__main__':   
     
-    #OPTION 1: use add_model_specific_args to create the dictionary of parameters:
+    #OPTION 1: use add_model_specific_args to pass arguments:
     parser = argparse.ArgumentParser(description="MPRAmodel", add_help=False)
-    parser = MPRAregressionModel.add_model_specific_args(parser)
-    paramDict = vars(parser.parse_args())   
-    model = MPRAregressionModel(paramDict)
+    parser = MPRAregressionModel.add_model_specific_args(parser) 
+    model = MPRAregressionModel(parser)
     summary(model, (model.numFeatures, model.seqLen) )
     
-    #OPTION 2: write manually a dictionary of parameters:
-    # modelParams_2 = {'LR':0.0005,
-    #                'momentum':0.9,
-    #                'weightDecay':1e-8,
-    #                'dropout':0.2,
-    #                'optimizer':'Adam',
-    #                'seqLen':600,
-    #                'numFeatures':4,
-    #                'targetLen':1,
-    #                'numChannles1':20,
-    #                'kernelSize1':12,
-    #                'stride1':3,
-    #                'padding1':0,
-    #                'dilation1':1,
-    #                'poolKernel1':8,
-    #                'poolStride1':2,
-    #                'numChannles2':10,
-    #                'kernelSize2':4,
-    #                'stride2':4,
-    #                'padding2':0,
-    #                'dilation2':1,
-    #                'poolKernel2':2,
-    #                'poolStride2':2,
-    #                'linearLayerLen1':30,
-    #                'linearLayerLen2':20 }    
-    # model_2 = MPRAregressionModel(modelParams_2)
+    #OPTION 2: pass the arguments manually:
+    # model = MPRAregressionModel(LR=0.0005,
+    #                             momentum=0.9,
+    #                             weightDecay=1e-8,
+    #                             dropout=0.2,
+    #                             optimizer='Adam',
+    #                             scheduler=True,
+    #                             seqLen=600,
+    #                             numFeatures=4,
+    #                             targetLen=1,
+    #                             numChannles1=20,
+    #                             kernelSize1=6,
+    #                             stride1=3,
+    #                             padding1=0,
+    #                             dilation1=1,
+    #                             poolKernel1=4,
+    #                             poolStride1=2,
+    #                             numChannles2=10,
+    #                             kernelSize2=4,
+    #                             stride2=2,
+    #                             padding2=0,
+    #                             dilation2=1,
+    #                             poolKernel2=2,
+    #                             poolStride2=2,
+    #                             linearLayerLen1=50,
+    #                             linearLayerLen2=10)
+    # summary(model, (model.numFeatures, model.seqLen) )
