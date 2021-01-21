0249ceed3753962fb520df6f0feb82512d7b7de5
Rodrigo Castro <rodrigo.i.castro@gmail.com>
2021-01-21 16:15:54 -0500

    Update FastSeqProp.py

--- boda/generator/FastSeqProp.py
+++ boda/generator/FastSeqProp.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 from warnings import warn
 
 
-class Fast_Seq_Prop(nn.Module):
+class FastSeqProp(nn.Module):
     def __init__(self,
                  num_sequences=1,
                  seq_len=200, 
@@ -25,7 +25,7 @@ class Fast_Seq_Prop(nn.Module):
                  downPad_DNA=None,
                  vocab_list=None,
                  **kwargs):
-        super(Fast_Seq_Prop, self).__init__()
+        super(FastSeqProp, self).__init__()
         self.num_sequences = num_sequences
         self.seq_len = seq_len  
         self.padding_len = padding_len
@@ -68,19 +68,26 @@ class Fast_Seq_Prop(nn.Module):
         self.scaleWeights = nn.Parameter(torch.rand((self.num_sequences, 4, 1)))
         self.shiftWeights = nn.Parameter(torch.rand((self.num_sequences, 4, 1)))
         
-    def create_paddingTensors(self):   
-        assert self.padding_len <= (len(self.upPad_DNA) + len(self.downPad_DNA)), 'Not enough padding available'
-        upPadTensor, downPadTensor = self.dna2tensor(self.upPad_DNA), \
-                                     self.dna2tensor(self.downPad_DNA)
-        upPadTensor, downPadTensor = upPadTensor[:,-self.padding_len//2 + self.padding_len%2:], \
-                                     downPadTensor[:,:self.padding_len//2 + self.padding_len%2]
-        upPadTensor, downPadTensor = upPadTensor.repeat(self.num_sequences, 1, 1), \
-                                     downPadTensor.repeat(self.num_sequences, 1, 1)
-        self.upPadTensor, self.downPadTensor = upPadTensor, downPadTensor
+    def create_paddingTensors(self):
+        assert self.padding_len >= 0 and type(self.padding_len) == int, 'Padding must be a nonnegative integer'
+        if self.padding_len > 0:
+            assert self.padding_len <= (len(self.upPad_DNA) + len(self.downPad_DNA)), 'Not enough padding available'
+            upPadTensor, downPadTensor = self.dna2tensor(self.upPad_DNA), \
+                                         self.dna2tensor(self.downPad_DNA)
+            upPadTensor, downPadTensor = upPadTensor[:,-self.padding_len//2 + self.padding_len%2:], \
+                                         downPadTensor[:,:self.padding_len//2 + self.padding_len%2]
+            upPadTensor, downPadTensor = upPadTensor.repeat(self.num_sequences, 1, 1), \
+                                         downPadTensor.repeat(self.num_sequences, 1, 1)
+            self.upPadTensor, self.downPadTensor = upPadTensor, downPadTensor
+        else:
+            self.upPadTensor, self.downPadTensor = None, None
     
     def pad(self, tensor):
-        paddedTensor = torch.cat([ self.upPadTensor, tensor, self.downPadTensor], dim=2)
-        return paddedTensor
+        if self.padding_len > 0:
+            paddedTensor = torch.cat([ self.upPadTensor, tensor, self.downPadTensor], dim=2)
+            return paddedTensor
+        else: 
+            return tensor
     
     def dna2tensor(self, sequence_str):
         seqTensor = np.zeros((len(self.vocab_list), len(sequence_str)))
@@ -89,7 +96,38 @@ class Fast_Seq_Prop(nn.Module):
         seqTensor = torch.Tensor(seqTensor)
         return seqTensor
     
-    def sample(self, padded=True):
+    def optimize(self, predictor, loss_fn, steps=20, learning_rate=0.5, step_print=5):
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=steps)
+        
+        print('-----Initial sequence(s)-----')
+        print(self.pad(self.trainable_sequences).detach().numpy())
+        print('-----Training steps-----')
+        loss_hist  = []
+        for step in range(1, steps+1):
+            optimizer.zero_grad()
+            softmaxed_sequences, sampled_sequences = self()
+            predictions = predictor(sampled_sequences)
+            loss = loss_fn(predictions)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            loss_hist.append(loss.item())
+            if step % step_print == 0:
+                print(f'step: {step}, loss: {round(loss.item(),6)}, learning_rate: {scheduler.get_last_lr()}')
+        
+        print('-----Final distribution-----')
+        print(self.padded_softmaxed_sequences.detach().numpy())
+
+        self.loss_hist = loss_hist
+        plt.plot(loss_hist)
+        plt.xlabel('Steps')
+        vert_label=plt.ylabel('Loss')
+        vert_label.set_rotation(90)
+        plt.show()
+        return None
+    
+    def generate(self, padded=True):
         if self.softmaxed_sequences == None:
             warn('The model hasn\'t been trained')
             return self.pad(self.trainable_sequences)
@@ -100,62 +138,33 @@ class Fast_Seq_Prop(nn.Module):
             sampled_sequences = torch.transpose(sampled_sequences_T, 1, 2)
             if padded:
                 sampled_sequences = self.pad(sampled_sequences)
-            return sampled_sequences
+            return sampled_sequences.detach()
 
 
 
 #--------------------------- EXAMPLE ----------------------------------------
 if __name__ == '__main__':
-    from Fast_Seq_Prop_utils import first_token_rewarder, neg_reward_loss
+    from FastSeqProp_utils import first_token_rewarder, neg_reward_loss
     import sys
     sys.path.insert(0, '/Users/castrr/Documents/GitHub/boda2/')    #edit path to boda2
     from boda.common import constants  
     
-    #np.random.seed(1)                   #anchor the initial DNA sequence(s)
-    #torch.manual_seed(1)                #anchor the sampling
-    np.set_printoptions(precision=2)    #for shorter display of np arrays
+    #np.random.seed(1)                   # anchor the initial DNA sequence(s)
+    #torch.manual_seed(1)                # anchor the sampling
+    #np.set_printoptions(precision=2)    # for shorter display of np arrays
     
-    numSequences = 1
-    seqLen = 3
-    paddingLen = 6
-    LR = 1
-    epochs = 10
+    model = FastSeqProp(num_sequences=1,
+                        seq_len=5,
+                        padding_len=2,
+                        upPad_DNA=constants.MPRA_UPSTREAM,
+                        downPad_DNA=constants.MPRA_DOWNSTREAM,
+                        vocab_list=constants.STANDARD_NT)
+    model.optimize(predictor=first_token_rewarder,
+                   loss_fn=neg_reward_loss,
+                   steps=20,
+                   learning_rate=0.5,
+                   step_print=5)
+    sample_example = model.generate()
     
-    FSP_model = Fast_Seq_Prop(num_sequences=numSequences,
-                              seq_len=seqLen,
-                              padding_len=paddingLen,
-                              upPad_DNA=constants.MPRA_UPSTREAM,
-                              downPad_DNA=constants.MPRA_DOWNSTREAM,
-                              vocab_list=constants.STANDARD_NT)
-    optimizer = torch.optim.Adam(FSP_model.parameters(), lr=LR, betas=(0.9, 0.999), eps=1e-8)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
-                                                           T_max=epochs, 
-                                                           eta_min=0.0000001, 
-                                                           last_epoch=-1)
-    
-    print('-----Initial sequence(s)-----')
-    print(FSP_model.pad(FSP_model.trainable_sequences).detach().numpy())   
-    reward_hist  = []
-    for epoch in range(1, epochs+1):
-        optimizer.zero_grad()
-        softmaxed_sequences, sampled_sequences = FSP_model()
-        predictions = first_token_rewarder(sampled_sequences)
-        loss = neg_reward_loss(predictions)
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
-        reward_hist.append(-loss.item())
-        if epoch%2==0:
-            print(f'epoch: {epoch}, reward: {round(-loss.item(),6)}') #, learning_rate: {scheduler.get_last_lr()}')
-            print('-----Updated sequence(s)-----')
-            print(softmaxed_sequences.detach().numpy()) 
-    
-    
-    print('-----Sampled example-----')
-    print(FSP_model.sample().detach().numpy())
-    plt.plot(reward_hist)
-    plt.xlabel('Gradient Steps')
-    vert_label=plt.ylabel('Reward')
-    vert_label.set_rotation(90)
-    plt.title('Reward per epoch')
-    plt.show()
+    print('-----Sample example-----')
+    print(sample_example.numpy())
