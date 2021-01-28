45d2011f92acd0ca9e3251ebf5bdb0d40772a63a
Rodrigo Castro <rodrigo.i.castro@gmail.com>
2021-01-28 13:55:16 -0500

    Update FastSeqProp.py

--- boda/generator/FastSeqProp.py
+++ boda/generator/FastSeqProp.py
@@ -11,7 +11,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
-import pytorch_lightning as pl
 import matplotlib.pyplot as plt
 from warnings import warn
 
@@ -20,10 +19,10 @@ class FastSeqProp(nn.Module):
     def __init__(self,
                  num_sequences=1,
                  seq_len=200, 
-                 padding_len=400,
+                 padding_len=0,
                  upPad_DNA=None,
                  downPad_DNA=None,
-                 vocab_list=None,
+                 vocab_list=['A','G','T','C'],
                  **kwargs):
         super(FastSeqProp, self).__init__()
         self.num_sequences = num_sequences
@@ -32,96 +31,93 @@ class FastSeqProp(nn.Module):
         self.upPad_DNA = upPad_DNA
         self.downPad_DNA = downPad_DNA
         self.vocab_list = vocab_list
+        self.vocab_len = len(vocab_list)
         self.create_paddingTensors()
         
-        #initialize the trainable tensors
-        self.create_random_trainable_sequences()
-        self.create_scaling_weights()       
-        self.softmaxed_sequences = None
+        #initialize the trainable logits
+        self.create_differentiable_input_logits()      
+        self.softmaxed_logits = None
+        
+        #instance normalization layer
+        self.instance_norm = nn.InstanceNorm1d(num_features=self.vocab_len, affine=True)
         
     def forward(self):
         #scaled softmax relaxation
-        normalized_sequences = F.instance_norm(self.trainable_sequences)
-        scaled_sequences = normalized_sequences * self.scaleWeights + self.shiftWeights
-        softmaxed_sequences = F.softmax(scaled_sequences, dim=1)
+        normalized_logits = self.instance_norm(self.differentiable_logits)
+        softmaxed_logits = F.softmax(normalized_logits, dim=1)
         #save attributes without messing the backward graph
-        self.softmaxed_sequences = softmaxed_sequences
-        self.padded_softmaxed_sequences = self.pad(softmaxed_sequences)
+        self.softmaxed_logits = softmaxed_logits
+        self.padded_softmaxed_logits = self.pad(softmaxed_logits)
         #sample
-        nucleotideProbs = Categorical(torch.transpose(softmaxed_sequences, 1, 2))
-        sampledIdxs = nucleotideProbs.sample()
-        sampled_sequences_T = F.one_hot(sampledIdxs, num_classes=4)        
-        sampled_sequences = torch.transpose(sampled_sequences_T, 1, 2)
-        sampled_sequences = sampled_sequences - softmaxed_sequences.detach() + softmaxed_sequences  #ST estimator trick
-        softmaxed_sequences, sampled_sequences = self.pad(softmaxed_sequences), self.pad(sampled_sequences)
-        return softmaxed_sequences, sampled_sequences
+        nucleotide_probs = Categorical(torch.transpose(softmaxed_logits, 1, 2))
+        sampled_idxs = nucleotide_probs.sample()
+        sampled_nucleotides_T = F.one_hot(sampled_idxs, num_classes=self.vocab_len)        
+        sampled_nucleotides = torch.transpose(sampled_nucleotides_T, 1, 2)
+        sampled_nucleotides = sampled_nucleotides - softmaxed_logits.detach() + softmaxed_logits  #ST estimator trick
+        softmaxed_logits, sampled_nucleotides = self.pad(softmaxed_logits), self.pad(sampled_nucleotides)
+        return softmaxed_logits, sampled_nucleotides
     
-    def create_random_trainable_sequences(self):
-        trainable_sequences = np.zeros((self.num_sequences, 4, self.seq_len))
+    def create_differentiable_input_logits(self):
+        differentiable_logits = np.zeros((self.num_sequences, self.vocab_len, self.seq_len))
         for seqIdx in range(self.num_sequences):
             for step in range(self.seq_len):
-                randomNucleotide = np.random.randint(4)
-                trainable_sequences[seqIdx, randomNucleotide, step] = 1
-        self.trainable_sequences = nn.Parameter(torch.tensor(trainable_sequences, dtype=torch.float))   
+                randomNucleotide = np.random.randint(self.vocab_len)
+                differentiable_logits[seqIdx, randomNucleotide, step] = 1
+        self.differentiable_logits = nn.Parameter(torch.tensor(differentiable_logits, dtype=torch.float))   
     
-    def create_scaling_weights(self):
-        self.scaleWeights = nn.Parameter(torch.rand((self.num_sequences, 4, 1)))
-        self.shiftWeights = nn.Parameter(torch.rand((self.num_sequences, 4, 1)))
-        
     def create_paddingTensors(self):
         assert self.padding_len >= 0 and type(self.padding_len) == int, 'Padding must be a nonnegative integer'
         if self.padding_len > 0:
             assert self.padding_len <= (len(self.upPad_DNA) + len(self.downPad_DNA)), 'Not enough padding available'
-            upPadTensor, downPadTensor = self.dna2tensor(self.upPad_DNA), \
+            upPad_logits, downPad_logits = self.dna2tensor(self.upPad_DNA), \
                                          self.dna2tensor(self.downPad_DNA)
-            upPadTensor, downPadTensor = upPadTensor[:,-self.padding_len//2 + self.padding_len%2:], \
-                                         downPadTensor[:,:self.padding_len//2 + self.padding_len%2]
-            upPadTensor, downPadTensor = upPadTensor.repeat(self.num_sequences, 1, 1), \
-                                         downPadTensor.repeat(self.num_sequences, 1, 1)
-            self.register_buffer('upPadTensor', upPadTensor)
-            self.register_buffer('downPadTensor', downPadTensor)
+            upPad_logits, downPad_logits = upPad_logits[:,-self.padding_len//2 + self.padding_len%2:], \
+                                         downPad_logits[:,:self.padding_len//2 + self.padding_len%2]
+            upPad_logits, downPad_logits = upPad_logits.repeat(self.num_sequences, 1, 1), \
+                                         downPad_logits.repeat(self.num_sequences, 1, 1)
+            self.register_buffer('upPad_logits', upPad_logits)
+            self.register_buffer('downPad_logits', downPad_logits)
         else:
-            self.upPadTensor, self.downPadTensor = None, None
+            self.upPad_logits, self.downPad_logits = None, None
 
     def pad(self, tensor):
         if self.padding_len > 0:
-            paddedTensor = torch.cat([ self.upPadTensor, tensor, self.downPadTensor], dim=2)
-            return paddedTensor
+            padded_tensor = torch.cat([ self.upPad_logits, tensor, self.downPad_logits], dim=2)
+            return padded_tensor
         else: 
             return tensor
     
     def dna2tensor(self, sequence_str):
-        seqTensor = np.zeros((len(self.vocab_list), len(sequence_str)))
+        seq_tensor = np.zeros((self.vocab_len, len(sequence_str)))
         for letterIdx, letter in enumerate(sequence_str):
-            seqTensor[self.vocab_list.index(letter), letterIdx] = 1
-        seqTensor = torch.Tensor(seqTensor)
-        return seqTensor
+            seq_tensor[self.vocab_list.index(letter), letterIdx] = 1
+        seq_tensor = torch.Tensor(seq_tensor)
+        return seq_tensor
     
     def optimize(self, predictor, loss_fn, steps=20, learning_rate=0.5, step_print=5, lr_scheduler=True):
-        if lr_scheduler: etaMin = 0.000001
-        else: etaMin = learning_rate
+        if lr_scheduler:
+            etaMin = 0.000001
+        else:
+            etaMin = learning_rate
         optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=steps, eta_min=etaMin)
-        
-        print('-----Initial sequence(s)-----')
-        print(self.pad(self.trainable_sequences).detach().numpy())
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=steps, eta_min=etaMin)      
+        print('-----Initial logits-----')
+        print(self.pad(self.differentiable_logits).detach().numpy())
         print('-----Training steps-----')
         loss_hist  = []
         for step in range(1, steps+1):
             optimizer.zero_grad()
-            softmaxed_sequences, sampled_sequences = self()
-            predictions = predictor(sampled_sequences)
+            softmaxed_logits, sampled_nucleotides = self()
+            predictions = predictor(sampled_nucleotides)
             loss = loss_fn(predictions)
             loss.backward()
             optimizer.step()
             scheduler.step()
             loss_hist.append(loss.item())
             if step % step_print == 0:
-                print(f'step: {step}, loss: {round(loss.item(),6)}, learning_rate: {scheduler.get_last_lr()}')
-        
+                print(f'step: {step}, loss: {round(loss.item(),6)}, learning_rate: {scheduler.get_last_lr()}')        
         print('-----Final distribution-----')
-        print(self.padded_softmaxed_sequences.detach().numpy())
-
+        print(self.padded_softmaxed_logits.detach().numpy())
         self.loss_hist = loss_hist
         plt.plot(loss_hist)
         plt.xlabel('Steps')
@@ -131,17 +127,17 @@ class FastSeqProp(nn.Module):
         return None
     
     def generate(self, padded=True):
-        if self.softmaxed_sequences == None:
+        if self.softmaxed_logits == None:
             warn('The model hasn\'t been trained yet')
-            return self.pad(self.trainable_sequences.detach())
+            return self.pad(self.differentiable_logits.detach())
         else:
-            nucleotideProbs = Categorical(torch.transpose(self.softmaxed_sequences, 1, 2))
-            sampledIdxs = nucleotideProbs.sample()
-            sampled_sequences_T = F.one_hot(sampledIdxs, num_classes=4)        
-            sampled_sequences = torch.transpose(sampled_sequences_T, 1, 2)
+            nucleotide_probs = Categorical(torch.transpose(self.softmaxed_logits, 1, 2))
+            sampled_idxs = nucleotide_probs.sample()
+            sampled_nucleotides_T = F.one_hot(sampled_idxs, num_classes=self.vocab_len)        
+            sampled_nucleotides = torch.transpose(sampled_nucleotides_T, 1, 2)
             if padded:
-                sampled_sequences = self.pad(sampled_sequences)
-            return sampled_sequences.detach()
+                sampled_nucleotides = self.pad(sampled_nucleotides)
+            return sampled_nucleotides.detach()
 
 
 
