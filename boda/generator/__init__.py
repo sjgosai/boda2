bb23dbbcfb32d4356a005760c902d345c0d0401f
sjgosai <sjgosai@g.harvard.edu>
2021-05-18 02:27:50 +0000

    org generator

--- boda/generator/__init__.py
+++ boda/generator/__init__.py
@@ -1,1 +1,7 @@
+from .nuts import NUTS3
+from .parameters import BasicParameters, StraightThroughParameters, GumbelSoftmaxParameters
 
+__all__ = [
+    'NUTS3', 
+    'BasicParameters', 'StraightThroughParameters', 'GumbelSoftmaxParameters'
+]
