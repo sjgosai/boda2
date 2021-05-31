aca8430b5135d1902e0bb7bf76442ce22d6d7a84
sjgosai <sjgosai@g.harvard.edu>
2021-05-31 20:07:53 +0000

    minor updates

--- boda/graph/utils.py
+++ boda/graph/utils.py
@@ -89,20 +89,20 @@ def add_scheduler_specific_args(parser, scheduler_name):
         group.add_argument('--eta_min', type=float, default=0.)
         group.add_argument('--last_epoch', type=int, default=-1)
     elif scheduler_name == 'ReduceLROnPlateau':
-        group.add_argument('--mode', type=str, default='min')
+        group.add_argument('--scheduler_mode', type=str, default='min')
         group.add_argument('--factor', type=float, default=0.1)
         group.add_argument('--patience', type=int, default=10)
         group.add_argument('--threshold', type=float, default=1e-4)
         group.add_argument('--threshold_mode', type=str, default='rel')
         group.add_argument('--cooldown', type=int, default=0)
         group.add_argument('--min_lr', type=float, default=0.)
-        group.add_argument('--eps', type=float, default=1e-8)
+        #group.add_argument('--eps', type=float, default=1e-8)
     elif scheduler_name == 'CyclicLR':
         group.add_argument('--base_lr', type=float, required=True)
         group.add_argument('--max_lr', type=float, required=True)
         group.add_argument('--step_size_up', type=int, default=2000)
         group.add_argument('--step_size_down', type=int)
-        group.add_argument('--mode', type=str, default='triangular')
+        group.add_argument('--scheduler_mode', type=str, default='triangular')
         group.add_argument('--gamma', type=float, default=1.0)
         group.add_argument('--scale_mode', type=str, default='cycle')
         group.add_argument('--cycle_momentum', type=cutils.str2bool, default=True)
@@ -149,6 +149,12 @@ def reorg_optimizer_args(optim_arg_dict):
         optim_arg_dict.pop('step_size2')
     return optim_arg_dict
 
+def reorg_scheduler_args(sched_arg_dict):
+    if 'scheduler_mode' in sched_arg_dict.keys():
+        sched_arg_dict['mode'] = sched_arg_dict['scheduler_mode']
+        sched_arg_dict.pop('scheduler_mode')
+    return sched_arg_dict
+
 def filter_state_dict(model, stashed_dict):
     results_dict = { 
         'filtered_state_dict': {},
@@ -186,13 +192,13 @@ def filter_state_dict(model, stashed_dict):
             
     return results_dict
                      
-def Pearson_correlation(x, y):
+def pearson_correlation(x, y):
     vx = x - torch.mean(x, dim=0)
     vy = y - torch.mean(y, dim=0)
     pearsons = torch.sum(vx * vy, dim=0) / (torch.sqrt(torch.sum(vx ** 2, dim=0)) * torch.sqrt(torch.sum(vy ** 2, dim=0)))
     return pearsons, torch.mean(pearsons)
     
-def Shannon_entropy(x):
+def shannon_entropy(x):
     p_c = nn.Softmax(dim=1)(x)    
     return torch.sum(- p_c * torch.log(p_c), axis=1)
 
@@ -203,13 +209,13 @@ def _get_ranks(x):
     if len(x.shape) > 1:
         dims = x.shape[1]
         for dim in range(dims):
-            ranks[tmp[:,dim], dim] = torch.arange(x.shape[0])
+            ranks[tmp[:,dim], dim] = torch.arange(x.shape[0], layout=x.layout, device=x.device)
     else:
-        ranks[tmp] = torch.arange(x.shape[0])
+        ranks[tmp] = torch.arange(x.shape[0], layout=x.layout, device=x.device)
     return ranks
 
 # Not useful for gpu
-def Spearman_correlation(x, y):
-    x_rank = _get_ranks(x).double()
-    y_rank = _get_ranks(y).double()
-    return Pearson_correlation(x_rank, y_rank)
+def spearman_correlation(x, y):
+    x_rank = _get_ranks(x).float()
+    y_rank = _get_ranks(y).float()
+    return pearson_correlation(x_rank, y_rank)
