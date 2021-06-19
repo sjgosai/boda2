a3124b73e037b1a900e5a810345782bbc11bb3fb
sjgosai <sjgosai@g.harvard.edu>
2021-06-19 16:03:52 +0000

    added branched models

--- boda/model/__init__.py
+++ boda/model/__init__.py
@@ -1,9 +1,9 @@
 from .basset import Basset, BassetVL
 from .mpra_basset import MPRA_Basset
-from .contrib import BassetEntropyVL
+from .contrib import BassetEntropyVL, BassetBranched
 
 __all__ = [
     'Basset', 'BassetVL', 
     'MPRA_Basset', 
-    'BassetEntropyVL'
+    'BassetEntropyVL', 'BassetBranched'
 ]
