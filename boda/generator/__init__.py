77d9e815d1ed54e598de6754eddc21c867bd7243
Rodrigo Castro <rodrigo.i.castro@gmail.com>
2021-09-21 16:19:51 -0400

    Update __init__.py

--- boda/generator/__init__.py
+++ boda/generator/__init__.py
@@ -1,6 +1,7 @@
 from .nuts import NUTS3, HMC, HMCDA
 from .parameters import BasicParameters, StraightThroughParameters, GumbelSoftmaxParameters
 from .FastSeqProp import FastSeqProp
+from .AdaLead import AdaLead
 
 __all__ = [
     'NUTS3', 'HMC', 'HMCDA', 
