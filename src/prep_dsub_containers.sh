28ac50d3d6f1b0cc4e7b7caf13308fdc004781c2
sjgosai <sgosai@g.harvard.edu>
2022-12-22 23:17:43 +0000

    fixed copypasta error

--- src/prep_dsub_containers.sh
+++ src/prep_dsub_containers.sh
@@ -3,19 +3,29 @@
 container_version=$1
 
 # install git
-sudo apt-get update
-sudo apt-get install git# install docker
-sudo apt-get install \
+apt-get update
+apt-get install git
+
+# install docker
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
-    lsb-releasecurl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpgecho \
+    lsb-release
+    
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io# make docker available with general user privilege
-sudo gpasswd -a $(whoami) docker# docker build, tag and push
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+# make docker available with general user privilege
+gpasswd -a $(whoami) docker
+
+# docker build, tag and push
 git clone https://github.com/sjgosai/boda2.git
 cd boda2
 docker build -f docker/dsub/Dockerfile -t dsub . --no-cache
