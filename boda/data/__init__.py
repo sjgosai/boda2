0cc92a13ab0765744f84bc49cdf2c834f29080bf
sjgosai <sgosai@g.harvard.edu>
2023-10-13 02:06:27 +0000

    clean up data submodule

--- boda/data/__init__.py
+++ boda/data/__init__.py
@@ -1,10 +1,9 @@
-from .mpra_data import BODA2_DataModule
 from .mpra_datamodule import MPRA_DataModule
 from .fasta_datamodule import FastaDataset, Fasta, VcfDataset, VCF
-from .contrib import SeqDataModule
+from .table_datamodule import SeqDataModule
 
 __all__ = [
-    'BODA2_DataModule',
     'MPRA_DataModule',
-    'Fasta', 'FastaDataset', 'VcfDataset', 'VCF', 'SeqDataModule'
+    'Fasta', 'FastaDataset', 'VcfDataset', 'VCF', 
+    'SeqDataModule'
 ]
--- boda/data/example.py
+++ boda/data/example.py
@@ -1,3 +1,10 @@
+r"""Example of a standardized module to process data for training models. 
+This is based on the spec from lightning.pytorch.core.datamodule (see 
+https://lightning.ai/docs for more information) with additional staticmethods 
+that can add arguments to an ArgumentParser which mirror class construction 
+arguments.
+"""
+
 import torch
 import argparse
 
@@ -5,6 +12,9 @@ class ExampleData(torch.nn.Module):
     @staticmethod
     def add_data_specific_args(parent_parser):
         r"""Argparse interface to expose class kwargs to the command line.
+        Required to specify for compatability with CLI. These args should 
+        mirror class construction arguments that are not determined 
+        conditionally.
         Args:
             parent_parser (argparse.ArgumentParser): A parent ArgumentParser to 
                 which more args will be added.
@@ -16,6 +26,21 @@ class ExampleData(torch.nn.Module):
         parser.add_argument('--batch_size', type=int)
         return parser
         
+    @staticmethod
+    def add_conditional_args(parser, known_args):
+        r"""Method to add conditional args after `add_data_specific args` 
+        sets primary arguments. Required to specify for CLI conpatability, 
+        but can return unmodified parser as in this example. These args should 
+        mirror class construction arguments that would be determined based on 
+        other arguments.
+        Args:
+            parser (argparse.AtgumentParser): an ArgumentParser to  which 
+                more args can be added.
+            known_args (Namespace): Known arguments that have been parsed 
+                by non-conditionally specified args.
+        """
+        return parser
+
     def __init__(self, data_dir='/no/data_dir/specified', batch_size=8, **kwargs):
         super().__init__()
         delf.data_name = 'ExampleData'
@@ -23,7 +48,7 @@ class ExampleData(torch.nn.Module):
         self.batch_size= batch_size
         
         self.unused_kwargs = kwargs
-        
+
     def prepare_data(self):
         r"""A method to prepare data based on class attributes, usually by 
         linking new attributes to `self`.
@@ -66,4 +91,3 @@ class ExampleData(torch.nn.Module):
         """
         raise NotImplementedError
         return Testing_DataLoader
-        
