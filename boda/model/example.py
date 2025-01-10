5e015e30970fd7ab8ad113974a0688c2e60faaab
Sager Gosai <20822509+sjgosai@users.noreply.github.com>
2025-01-10 10:46:14 -0500

    Update example.py

--- boda/model/example.py
+++ boda/model/example.py
@@ -1,7 +1,7 @@
 r"""
 MIT License
 
-Copyright (c) 2025 Sagar Gosai
+Copyright (c) 2025 Sagar Gosai, Rodrigo Castro
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
